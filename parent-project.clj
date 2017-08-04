67f7204e8c70335365438eed0653b517daf2e935
Jarppe <jarppe@metosin.fi>
2017-08-04 18:10:32 +0300

    All in one dir

---
+++ parent-project.clj
@@ -0,0 +1,2 @@
+(defproject test-refresh-managed-deps-issue "parent"
+  :managed-dependencies [[clj-time "0.14.0"]])
--- project.clj
+++ project.clj
@@ -1,2 +1,7 @@
-(defproject test-refresh-managed-deps-issue "parent"
-  :managed-dependencies [[clj-time "0.14.0"]])
+(defproject test-refresh-managed-deps-issue/app "0.1.0-SNAPSHOT"
+  :plugins [[lein-parent "0.3.1"]]
+  :parent-project {:path "./parent-project.clj"
+                   :inherit [:managed-dependencies]}
+
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
+                 [clj-time]])
